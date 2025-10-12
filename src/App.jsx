20da409e17f517a453a2c5b80b714310bea8e56e
ا/محمود جاد مصطفي <mahmoudgadmostafa@gmail.com>
2025-10-12 23:18:45 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -33,6 +33,14 @@ const ProtectedRoute = ({ allowedRoles }) => {
     );
   }
 
+  if (user && user.role === undefined) {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-b-4 border-primary"></div>
+    </div>
+  );
+}
+
   if (!isAuthenticated) {
     return <Navigate to="/login" state={{ from: location }} replace />;
   }
